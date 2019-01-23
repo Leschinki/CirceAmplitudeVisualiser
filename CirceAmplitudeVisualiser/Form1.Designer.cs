c1e16e4b502cd23f46ae8d71b785bf3bd65dc027
Stefan Lewicki <stefan.lewicki@ms.bulme.at>
2019-01-23 17:10:29 +0100

    Version 0.5

--- CirceAmplitudeVisualiser/Form1.Designer.cs
+++ CirceAmplitudeVisualiser/Form1.Designer.cs
@@ -29,12 +29,82 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this._panel = new System.Windows.Forms.Panel();
+            this._cbDevice = new System.Windows.Forms.ComboBox();
+            this._trbMulti = new System.Windows.Forms.TrackBar();
+            this._timer = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this._trbMulti)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // _panel
+            // 
+            this._panel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this._panel.Location = new System.Drawing.Point(13, 65);
+            this._panel.Name = "_panel";
+            this._panel.Size = new System.Drawing.Size(775, 373);
+            this._panel.TabIndex = 0;
+            this._panel.Paint += new System.Windows.Forms.PaintEventHandler(this.OnPanelPaint);
+            this._panel.Resize += new System.EventHandler(this.OnPanelResize);
+            // 
+            // _cbDevice
+            // 
+            this._cbDevice.FormattingEnabled = true;
+            this._cbDevice.Location = new System.Drawing.Point(13, 21);
+            this._cbDevice.Name = "_cbDevice";
+            this._cbDevice.Size = new System.Drawing.Size(121, 21);
+            this._cbDevice.TabIndex = 1;
+            // 
+            // _trbMulti
+            // 
+            this._trbMulti.Location = new System.Drawing.Point(140, 12);
+            this._trbMulti.Maximum = 5;
+            this._trbMulti.Minimum = 1;
+            this._trbMulti.Name = "_trbMulti";
+            this._trbMulti.Size = new System.Drawing.Size(104, 45);
+            this._trbMulti.TabIndex = 2;
+            this._trbMulti.Value = 1;
+            // 
+            // _timer
+            // 
+            this._timer.Interval = 10;
+            this._timer.Tick += new System.EventHandler(this.OnTimerTick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(271, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this._trbMulti);
+            this.Controls.Add(this._cbDevice);
+            this.Controls.Add(this._panel);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this._trbMulti)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel _panel;
+        private System.Windows.Forms.ComboBox _cbDevice;
+        private System.Windows.Forms.TrackBar _trbMulti;
+        private System.Windows.Forms.Timer _timer;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- CirceAmplitudeVisualiser/Form1.cs
+++ CirceAmplitudeVisualiser/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NAudio.CoreAudioApi;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -8,13 +9,64 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace CirceAmplitudeVisualiser
 {
     public partial class Form1 : Form
     {
+        public const int POINTSCOUNT = 360;
+        private PointF[] points = new PointF[POINTSCOUNT];
+        int currentPoint = 0;
         public Form1()
         {
             InitializeComponent();
+            setup();
+            _timer.Enabled = true;
+        }
+
+        private void setup()
+        {
+            MMDeviceEnumerator enumerator = new MMDeviceEnumerator();
+            var devices = enumerator.EnumerateAudioEndPoints(DataFlow.All, DeviceState.Active);
+            _cbDevice.Items.AddRange(devices.ToArray());
+            if (_cbDevice.Items.Count > 0)
+                _cbDevice.SelectedIndex = 0;
+        }
+        public static PointF PointOnCircle(float radius, float angleInDegrees, PointF origin)
+        {
+            // Convert from degrees to radians via multiplication by PI/180        
+            float x = (float)(radius * Math.Cos(angleInDegrees * Math.PI / 180F)) + origin.X;
+            float y = (float)(radius * Math.Sin(angleInDegrees * Math.PI / 180F)) + origin.Y;
+
+            return new PointF(x, y);
+        }
+
+        private void OnPanelPaint(object sender, PaintEventArgs e)
+        {
+            Graphics gr = e.Graphics;
+            Pen pen = new Pen(Brushes.Blue, 3);
+            for(int i = 0; i < POINTSCOUNT; i++)
+            {
+                gr.DrawLine(pen, points[i], points[(i + 1) % 360]);
+            }
+        }
+
+        private void OnTimerTick(object sender, EventArgs e)
+        {
+            _timer.Enabled = false;
+            Point middle = new Point(_panel.Width / 2, _panel.Height / 2);
+            float volume = ((MMDevice)(_cbDevice.SelectedItem)).AudioMeterInformation.MasterPeakValue * 10000 * _trbMulti.Value + 50 * _trbMulti.Value;
+            label1.Text = volume.ToString();
+            currentPoint %= POINTSCOUNT;
+            points[currentPoint++] = PointOnCircle(volume, currentPoint * (360/POINTSCOUNT), middle);
+            _panel.Invalidate();
+            _timer.Enabled = true;
+        }
+
+        private void OnPanelResize(object sender, EventArgs e)
+        {
+            Array.Clear(points, 0, POINTSCOUNT);
+            _panel.Invalidate();
         }
     }
 }
